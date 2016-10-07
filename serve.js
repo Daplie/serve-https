a33084d8168003ccfefeeaaa4fd56dabce63f9e8
AJ ONeal <awesome@coolaj86.com>
2016-10-06 22:28:05 -0600

    fix matching ips and httpolyglot

--- serve.js
+++ serve.js
@@ -81,6 +81,7 @@ function createServer(port, pubdir, content, opts) {
       if ('false' !== opts.insecurePort && httpPort !== opts.insecurePort) {
         return createInsecureServer(opts.insecurePort, pubdir, opts).then(resolve);
       } else {
+        opts.insecurePort = opts.port;
         resolve();
       }
     });
@@ -270,8 +271,8 @@ function run() {
         if (!matchingIps.length) {
           console.log("Neither the attached nor external interfaces match '" + argv.servername + "'");
         }
-        opts.matchingIps = matchingIps || [];
       }
+      opts.matchingIps = matchingIps || [];
 
       if (opts.matchingIps.length) {
         console.info('');
